e60d4c045ff8203a7cdb68bd51ba7e039b6ca654
Kakonoomoide <rizkiramada128@gmail.com>
2023-05-25 12:17:30 +0700

    menambahkan switch case

--- double_linked_list.cpp
+++ double_linked_list.cpp
@@ -179,9 +179,36 @@ int main(){
 			cout << "4. View all records in the descending order of roll numbers" << endl;
 			cout << "5. Search for a record in the list" << endl;
 			cout << "6. Exit" << endl;
+
+			cout << "\nEnter your choice (1-6): ";
+			char ch;
+			cin >> ch;
+
+			switch (ch){
+			case'1':
+				obj.addNode();
+				break;
+			case'2':
+				obj.hapus();
+				break;
+			case'3':
+				obj.ascending();
+				break;
+			case'4':
+				obj.descending();
+				break;
+			case'5':
+				obj.searchData();
+				break;
+			case'6':
+				return 0;
+			default:
+				cout << "\ninvalid option" << endl;
+				break;
+			}
 		}
 		catch (exception e) {
-			cout << "Terjadi Kesalahan" << endl;
+			cout << "Check for the values entered" << endl;
 		}
 	}
 }
