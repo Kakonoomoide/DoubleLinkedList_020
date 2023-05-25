d121b6f8ebf519c776845d1c0f0b389879459ab1
Kakonoomoide <rizkiramada128@gmail.com>
2023-05-25 10:13:32 +0700

    Add project files.

---
+++ double_linked_list.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+	int noMhs;
+	string name;
+	Node* next;
+	Node* prev;
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
