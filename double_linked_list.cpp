50d41d6603fbefc9c0d024f8fe31cc864e12e974
Kakonoomoide <rizkiramada128@gmail.com>
2023-05-25 11:43:56 +0700

    fix kondisi ke dua

--- double_linked_list.cpp
+++ double_linked_list.cpp
@@ -120,14 +120,18 @@ void DoubleLinkedList::descending() {
 		cout << "\nlist is empty" << endl;
 
 	else {
-		cout << "\nRecord in ascending order of roll number are: " << endl;
+		cout << "\nRecord in descending order of roll number are: " << endl;
 		Node* currentNode = START;
-		while (currentNode != NULL) {
-			cout << currentNode->noMhs << " " << currentNode->name << endl;
+		while (currentNode->next != NULL) {
 			currentNode = currentNode->next;
+		}
+		while (currentNode!=NULL){
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
 		}
 	}
 }
+
 
 int main()
 {
