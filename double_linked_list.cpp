7736396aded06cfb4a761d0cbbea938b59eee0a2
Kakonoomoide <rizkiramada128@gmail.com>
2023-05-25 11:34:53 +0700

    kondisi kedua list memiliki data

--- double_linked_list.cpp
+++ double_linked_list.cpp
@@ -118,6 +118,15 @@ void DoubleLinkedList::ascending() {
 void DoubleLinkedList::descending() {
 	if (listEmpty())
 		cout << "\nlist is empty" << endl;
+
+	else {
+		cout << "\nRecord in ascending order of roll number are: " << endl;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+	}
 }
 
 int main()
