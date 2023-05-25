c88d3bdc535ba5c6899c0203b4bc4c275e67d42b
Kakonoomoide <rizkiramada128@gmail.com>
2023-05-25 12:00:09 +0700

    menambahkan methode untuk mencari data

--- double_linked_list.cpp
+++ double_linked_list.cpp
@@ -147,7 +147,27 @@ void DoubleLinkedList::hapus() {
 		cout << "Record with roll number " << rollNo << " deleted" << endl;
 }
 
-int main()
-{
-    std::cout << "Hello World!\n";
+void DoubleLinkedList::searchData() {
+	if (listEmpty()==true)
+		cout << "\nlist is empty" << endl;
+
+	Node* prev, * curr;
+	prev = curr = NULL;
+
+	cout << "\nEnter the roll number of the student whose record you want search: ";
+	int num;
+	cin >> num;
+
+	if (DoubleLinkedList::search(num,&prev,&curr) == false)
+		cout << "\nRecord not found" << endl;
+	else{
+		cout << "\nRecord found" << endl;
+		cout << "\nRoll number: " << curr->noMhs << endl;
+		cout << "\nName: " << curr->name << endl;
+
+	}
+}
+
+int main(){
+
 }
